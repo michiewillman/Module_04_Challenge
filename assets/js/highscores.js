1ff1d08eb2786e150e5b3a1c3fa22d8b21c2c381
Michie <contact@michiewillman.com>
2023-05-25 00:17:23 -0700

    Rendering highscores fixed

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,21 +1,18 @@
 var scoreList = document.getElementById("highscores");
+let highScoresTag = '';
 
 // Set function to pull scores from local storage
 function renderHighScores() {
   // Pull all user scores from storage & parse
-  localStorage.getItem("allStoredScores", JSON.stringify(allStoredScores));
+  var highScores = JSON.parse(localStorage.getItem("allStoredScores"));
 
-  if (allStoredScores !== null) {
-    // Display initials & scores
-    for (var i = 0; i > allStoredScores.length; i++) {
-      scoreList = allStoredScores[i];
-
-      // Make li child for every score pairing from storage
-      var li = document.createElement("li");
-      scoreList.appendChild(li)
-    }
+  for (var i = 0; i < localStorage.length; i++) {
+    var userEach = document.createElement("li");
+    scoreList.appendChild(userEach);
+    userEach.textContent = highScores.initials + highScores.score;
   }
 }
+
 // Call page to automatically pull scores from local storage
 renderHighScores();
 
